af7b376d03da7ede39d9594db11be2cda956afff
Timothy Chan Yan Mong <timothychan29475600@gmail.com>
2023-08-26 21:31:23 -0400

    Ignore the .txt files generated

---
+++ plots/s3-run.sh
@@ -0,0 +1,4 @@
+python s3-long-multiproc-run.py  --in-path ../athenaMLdata/data/TurbPar.out2.00103.athdf --out-prefix ./s3long-out/s3-normal-multiproc-bash --r-bins "1/128,1,50" --nproc 5 --nsamples 500000
+python s3-long-multiproc-run.py  --in-path ../athenaMLdata/data_dedt5e-4/TurbPar.out2.00010.athdf --out-prefix ./s3long-out/s3-dedt5e4-multiproc-bash --r-bins "1/128,1,50" --nproc 5 --nsamples 500000
+python s3-long-multiproc-run.py  --in-path ../athenaMLdata/data_kdrive3-8/TurbPar.out2.00010.athdf --out-prefix ./s3long-out/s3-kdrive38-multiproc-bash --r-bins "1/128,1,50" --nproc 5 --nsamples 500000
+python s3-long-multiproc-run.py  --in-path ../athenaMLdata/data_ts0.001/TurbPar.out2.00010.athdf --out-prefix ./s3long-out/s3-ts001-multiproc-bash --r-bins "1/128,1,50" --nproc 5 --nsamples 500000
